9936cc8c1fb644c8b7bb51e3876892450f9ddc70
yzy <1187997355@qq.com>
2017-07-31 00:53:04 +0800

    PhysicsBasedAnimationsDemo

---
+++ PhysicsBasedAnimationsDemo/app/src/main/java/com/example/yzy/physicsbasedanimationsdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.yzy.physicsbasedanimationsdemo;
+
+import android.support.animation.DynamicAnimation;
+import android.support.animation.SpringAnimation;
+import android.support.animation.SpringForce;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View mIvBall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mIvBall = findViewById(R.id.iv_ball);
+
+        findViewById(R.id.btn_spring).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final SpringAnimation springAnimation = new SpringAnimation(mIvBall, DynamicAnimation.X);
+                final SpringForce springForce = new SpringForce();
+                springForce.setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY);
+                springForce.setStiffness(SpringForce.STIFFNESS_VERY_LOW);
+                springForce.setFinalPosition(mIvBall.getX());
+                springAnimation.setSpring(springForce);
+                springAnimation.setStartVelocity(5000);
+                springAnimation.start();
+            }
+        });
+    }
+}

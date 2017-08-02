d7a9559900f74861232999c8b65dc72facde730e
yzy <1187997355@qq.com>
2017-08-02 14:34:44 +0800

    ViewPagerDemo

---
+++ ViewPagerDemo/app/src/main/java/com/example/yzy/viewpagerdemo/MyFragmentPagerAdapter.java
@@ -0,0 +1,27 @@
+package com.example.yzy.viewpagerdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by yzy on 2017/8/1.
+ */
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public MyFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+}
